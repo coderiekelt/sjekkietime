48c28649696d31f022bc73236db82a0653dff104
Riekelt Brands <r.brands@otakusquare.com>
2020-03-13 00:51:20 +0100

    Fixes

--- src/SjekkieTime.php
+++ src/SjekkieTime.php
@@ -15,7 +15,7 @@ class SjekkieTime
             die(file_get_contents($this->directory . '/templates/' . $this->schedules[$schedule]->getTemplate()));
         }
 
-        die('<center><h1>nee :(</h1></center>');
+        die('<center><img src="https://st2.depositphotos.com/1049184/6926/i/950/depositphotos_69269213-stock-photo-sad-man-in-scarf-have.jpg" alt="Nee" style="height: 100%;"></center>');
     }
 
     public function load(string $directory) {
